5f144723054d2671c950abd95223e1aca22c9d65
Vinodh Kumar <vinodh.mahendra@gmail.com>
2020-10-17 15:28:50 +0530

    CI/CD JUnit Sample Program

---
+++ src/test/java/com/simplilearn/workshop/JenkinsJUnit.java
@@ -0,0 +1,31 @@
+package com.simplilearn.workshop;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class JenkinsJUnit {
+
+	@Test
+	public void testCasePassed() {
+		int a = 1;
+		int b = 2;
+		Assert.assertTrue(a + b == 3);
+	}
+
+	@Test
+	public void testCaseFailed() {
+		int a = 1;
+		int b = 2;
+		Assert.assertTrue(a + b == 4);
+	}
+
+	@Ignore
+	@Test
+	public void testCaseIgnored() {
+		fail("this test fail");
+	}
+
+}
